27764d91217e465207e60a862517243991307565
angrifel <angrifel@users.noreply.github.com>
2017-04-26 14:28:33 -0300

    added script for bootstraping docker

---
+++ docker/docker-01-setup-repository.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+apt-get -y install \
+  apt-transport-https \
+  ca-certificates \
+  curl
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+
+apt-get update
