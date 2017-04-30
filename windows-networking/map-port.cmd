d5c5858a073062818edf5257a032c99388115b4f
angrifel <angrifel@users.noreply.github.com>
2017-04-29 21:11:03 -0300

    fixed bug in map-port.cmd script that terminated innermost session

--- windows-networking/map-port.cmd
+++ windows-networking/map-port.cmd
@@ -13,31 +13,31 @@ shift
 if "%LISTEN_IP%"=="" (
     echo Listen IP is missing.
     echo %USAGE_TEXT%
-    exit 1 /B
+    exit /B 1
 )
 
 if "%LISTEN_PORT%"=="" (
     echo Listen Port is missing.
     echo %USAGE_TEXT%
-    exit 1 /B
+    exit /B 1
 )
 
 if "%CONNECT_IP%"=="" (
     echo Connect IP is missing.
     echo %USAGE_TEXT%
-    exit 1 /B
+    exit /B 1
 )
 
 if "%CONNECT_PORT%"=="" (
     echo Connect Port is missing.
     echo %USAGE_TEXT%
-    exit 1 /B
+    exit /B 1
 )
 
 if not "%1"=="" (
     echo Only 4 parameters should be supplied.
     echo %USAGE_TEXT%
-    exit 1 /B
+    exit /B 1
 )
 
 netsh interface portproxy add v4tov4 listenport=%LISTEN_PORT% listenaddress=%LISTEN_IP% connectport=%CONNECT_PORT% connectaddress=%CONNECT_IP%
